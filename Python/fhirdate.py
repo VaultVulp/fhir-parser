a96e07e8492fb7c0b52612cc96ab040d8d0ded1d
VaultVulp <flammeusvulpes@gmail.com>
2017-04-04 14:11:19 +0600

    Reworked FHIRDate presentation.

--- Python/fhirdate.py
+++ Python/fhirdate.py
@@ -7,6 +7,7 @@
 import sys
 import logging
 import isodate
+
 from datetime import date, datetime
 
 
@@ -18,40 +19,68 @@ class FHIRDate(object):
 
     def __init__(self, jsonval=None):
         self.date = None
+        self._format = None
+
         if jsonval is not None:
             if isinstance(jsonval, (date, datetime)):
                 self.date = jsonval
             else:
                 isstr = isinstance(jsonval, str)
-                if not isstr and sys.version_info[0] < 3:  # Python 2.x has 'str' and 'unicode'
+                # Python 2.x has 'str' and 'unicode'
+                if not isstr and sys.version_info[0] < 3:
                     isstr = isinstance(jsonval, basestring)
                 if not isstr:
                     raise TypeError(
-                        "Expecting string or datetime when initializing {}, but got {}"
-                        .format(type(self), type(jsonval)))
+                        "Expecting string or datetime when initializing {}, "
+                        "but got {}".format(type(self), type(jsonval))
+                    )
                 try:
                     if 'T' in jsonval:
                         self.date = isodate.parse_datetime(jsonval)
                     else:
                         self.date = isodate.parse_date(jsonval)
                 except Exception as e:
-                    logging.warning("Failed to initialize FHIRDate from \"{}\": {}"
-                                    .format(jsonval, e))
+                    logging.warning(
+                        "Failed to initialize FHIRDate from \"{}\": {}"
+                            .format(jsonval, e)
+                    )
 
-                self.origval = jsonval
+                self._origval = jsonval
 
     def __setattr__(self, prop, value):
         if 'date' == prop:
-            self.origval = None
+            self._format = None
+            self._origval = None
         object.__setattr__(self, prop, value)
+
+    @property
+    def format(self):
+        if self._format is None and self._origval:
+            self._format = self._datetime_format(self._origval)
+        return self._format
 
     @property
     def isostring(self):
         if self.date is None:
             return None
-        if isinstance(self.date, datetime):
+
+        if self.format is not None:
+            if '.%f' not in self.format:
+                result = isodate.datetime_isoformat(self.date, self.format)
+            else:
+                # Replace microseconds with milliseconds
+                formats = self.format.split('.%f')
+                result = isodate.datetime_isoformat(self.date, formats[0])
+                result += isodate.datetime_isoformat(self.date, '.%f')[:-3]
+                result += isodate.datetime_isoformat(self.date, formats[1])
+            if self._origval and self._origval.endswith('+00:00'):
+                # Restore original timezone presentation
+                result = result.replace('Z', '+00:00')
+            return result
+        elif isinstance(self.date, datetime):
             return isodate.datetime_isoformat(self.date)
-        return isodate.date_isoformat(self.date)
+        else:
+            return isodate.date_isoformat(self.date)
 
     @classmethod
     def with_json(cls, jsonobj):
@@ -73,6 +102,52 @@ class FHIRDate(object):
         return cls.with_json(jsonobj)
 
     def as_json(self):
-        if self.origval is not None:
-            return self.origval
         return self.isostring
+
+    def _date_format(self, date_string):
+        for pattern in isodate.isodates.build_date_regexps():
+            match = pattern.match(date_string)
+            if match:
+                groups = match.groupdict()
+                if 'century' in groups:
+                    return '%C'
+                elif 'month' not in groups:
+                    if 'week' in groups:
+                        if 'day' in groups:
+                            return '%Y %W %d'
+                        else:
+                            return '%Y %W'
+                    elif 'day' in groups:
+                        return '%Y %d'
+                    else:
+                        return '%Y'
+                elif 'day' not in groups or groups['day'] is None:
+                    return '%Y-%m'
+                else:
+                    return '%Y-%m-%d'
+
+    def _time_format(self, time_string):
+        if time_string:
+            for pattern in isodate.isotime.build_time_regexps():
+                match = pattern.match(time_string)
+                if match:
+                    groups = match.groupdict()
+                    if 'second' in groups:
+                        if '.' in groups['second']:
+                            return '%H:%M:%S.%f%Z'
+                        else:
+                            return '%H:%M:%S%Z'
+                    elif 'minute' in groups:
+                        return '%H:%M%Z'
+                    else:
+                        return '%H:%M%Z'
+
+    def _datetime_format(self, datetime_string):
+        if 'T' in datetime_string:
+            date_string, time_string = datetime_string.split('T')
+            time_format = self._time_format(time_string)
+        else:
+            date_string = datetime_string
+            time_format = ''
+        date_format = self._date_format(date_string)
+        return date_format + ('T' + time_format if time_format else '')
--- Python/tests/test_fhir_date.py
+++ Python/tests/test_fhir_date.py
@@ -3,6 +3,8 @@
 from unittest import TestCase, main
 from datetime import date, datetime
 
+from isodate import FixedOffset
+
 from ..resource import Resource
 from ..fhirdate import FHIRDate
 
@@ -10,24 +12,42 @@ from ..fhirdate import FHIRDate
 class CastDateTestCase(TestCase):
     def test_from_valid_str_datetime(self):
         el = FHIRDate('1986-09-21T021234')
-        self.assertEqual(el.as_json(), '1986-09-21T021234')
+        self.assertEqual(el.as_json(), '1986-09-21T02:12:34')
+        self.assertEqual(el.date, datetime(1986, 9, 21, 2, 12, 34))
 
         el = FHIRDate('19860921T02:12:34')
+        self.assertEqual(el.as_json(), '1986-09-21T02:12:34')
         self.assertEqual(el.date, datetime(1986, 9, 21, 2, 12, 34))
+
+        el = FHIRDate('2015-02-09T11:04:15.817-05:00')
+        self.assertEqual(el.as_json(), '2015-02-09T11:04:15.817-05:00')
+        self.assertEqual(
+            el.date,
+            datetime(2015, 2, 9, 11, 4, 15, 817000,  tzinfo=FixedOffset(-5))
+        )
+
+        el = FHIRDate('2013-01-01T10:50:00+00:00')
+        self.assertEqual(el.as_json(), '2013-01-01T10:50:00+00:00')
+        self.assertEqual(
+            el.date,
+            datetime(2013, 1, 1, 10, 50,tzinfo=FixedOffset())
+        )
 
     def test_from_valid_str_date(self):
         el = FHIRDate('19860921')
-        self.assertEqual(el.as_json(), '19860921')
-
-        el = FHIRDate('1986-09-21')
+        self.assertEqual(el.as_json(), '1986-09-21')
         self.assertEqual(el.date, date(1986, 9, 21))
 
-    def test_from_pratial_str_date(self):
+    def test_from_partial_str_date(self):
         el = FHIRDate('1986-09')
         self.assertEqual(el.as_json(), '1986-09')
-
-        el = FHIRDate('1986-09')
         self.assertEqual(el.date, date(1986, 9, 1))
+        self.assertEqual(el.format, '%Y-%m')
+
+        el = FHIRDate('1986')
+        self.assertEqual(el.as_json(), '1986')
+        self.assertEqual(el.date, date(1986, 1, 1))
+        self.assertEqual(el.format, '%Y')
 
     def test_from_obj_date(self):
         el = FHIRDate(date(1986, 9, 21))
@@ -37,9 +57,9 @@ class CastDateTestCase(TestCase):
         el = FHIRDate(date(1986, 9, 21))
         self.assertEqual(el.as_json(), '1986-09-21')
 
-    def test_from_not_valid_str_date(self):
+    def test_from_invalid_str_date(self):
         el = FHIRDate('1986-21-21')
-        self.assertEqual(el.as_json(), '1986-21-21')
+        self.assertEqual(el.as_json(), None)
 
 
 class CastFromDict(TestCase):
